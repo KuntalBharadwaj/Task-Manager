d69aa7221905da01573515d1c81b5e703bcfbb7d
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2024-11-26 10:45:24 +0530

    intializing the project

---
+++ src/utils/firebase.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCZyxZiXPtvh4iyYvqfVnCboQ-SOsdZwM",
+  authDomain: "task-manager-56241.firebaseapp.com",
+  projectId: "task-manager-56241",
+  storageBucket: "task-manager-56241.firebasestorage.app",
+  messagingSenderId: "155150069777",
+  appId: "1:155150069777:web:abc514cf37aab3bf8d81b0"
+};
+
+const app = initializeApp(firebaseConfig);
